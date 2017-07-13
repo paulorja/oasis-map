bdfb2810586b6bc1ace06e57a3379cd1081370d5
PAULO JANSEN <pauloo.jansen@gmail.com>
2017-07-13 02:13:43 -0300

    chubby glasses

--- lib/authentication.rb
+++ lib/authentication.rb
@@ -18,6 +18,8 @@ class Authentication
       player.character.inventory.add world.items[11]
       player.character.inventory.add world.items[12]
       player.character.inventory.add world.items[13]
+  	  player.character.inventory.add world.items[14]
+	  player.character.inventory.add world.items[15]
 
       server.send ClientMessages.inventory(player.character.inventory), ws
       server.send ClientMessages.init_world(world.height, world.width, world.part_of_world(0, 0, 10)), ws
