798df50f296280967d09e661ff7aae1e133af71c
PAULO JANSEN <pauloo.jansen@gmail.com>
2018-04-14 01:42:29 -0300

    worlds

--- lib/world_loader.rb
+++ lib/world_loader.rb
@@ -2,7 +2,9 @@ class WorldLoader
 
   def self.load_world
     begin
-      world_json = JSON.parse(File.read('./world/default_world.json'))
+      world_name = "default_world"
+      #world_name = "default_world_50npcs"
+      world_json = JSON.parse(File.read("./world/#{world_name}.json"))
       Log.log 'world load ok'
       return world_json
     rescue
