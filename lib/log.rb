6a34983f0a4837247ca1d2e03f8700631ff19789
PAULO JANSEN <pauloo.jansen@gmail.com>
2018-04-17 20:08:26 -0300

    logs

--- lib/log.rb
+++ lib/log.rb
@@ -1,27 +1,35 @@
 class Log
 
 	def self.log(msg)
-		puts "#{time_now} #{msg}"
+    info(msg)
 	end
 
+  def self.info(msg)
+		puts "[INFO]#{time_now}| #{msg}"
+  end
+
 	def self.alert(msg)
-		puts "#{time_now} #{'ALERT'.red} #{msg}"
+		puts "[ALERT]#{time_now}| #{msg}"
 	end
 
 	def self.debug(msg)
-		puts "#{time_now} #{'DEBUG'.blue}  #{msg}"
+		puts "[DEBUG]#{time_now}| #{msg}"
+	end
+
+	def self.error(msg)
+		puts "[ERROR]#{time_now}| #{msg}"
 	end
 
 	def self.send(msg)
-		puts "#{time_now} #{'SEND'.green} #{msg} "
+		puts "#{'[SEND]'.green}#{time_now}| #{msg} "
 	end
 
 	def self.push(msg)
-		puts "#{time_now} #{'PUSH'.green} #{msg} "
+		puts "#{'[PUSH]'.green}#{time_now}| #{msg} "
 	end
 
 	def self.time_now
-		"#{Time.now.strftime('%T')} #{Time.now.usec}"
+		"[#{Time.now.strftime('%T')} #{Time.now.usec}]"
 	end
 
 end
